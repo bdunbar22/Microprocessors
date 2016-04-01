f4bb6d69bebf971cc44c8b1f9882cef0cc4bab26
Ben Dunbar <dunbarb@ccs.neu.edu>
2016-04-01 11:59:23 -0400

    Adding audio output header

---
+++ ZedBoard_FreeRTOS_WorkSpace_V3_1_1/Operation_DAD/src/Audio_Output.h
@@ -0,0 +1,13 @@
+/*
+ * Audio_Output.h
+ *
+ *  Created on: Apr 1, 2016
+ *      Author: Ben
+ */
+
+#ifndef AUDIO_OUTPUT_H_
+#define AUDIO_OUTPUT_H_
+
+void outputFound(int sourceAngle, int distance);
+
+#endif /* AUDIO_OUTPUT_H_ */
